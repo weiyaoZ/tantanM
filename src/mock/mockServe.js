4d7e9e44f9ebd64cee3be57e7e22fa1333edcc76
liangchen926 <10736557+liangchen926@user.noreply.gitee.com>
2022-06-22 16:49:12 +0000

    mock

---
+++ src/mock/mockServe.js
@@ -0,0 +1,17 @@
+// 先引入 mockjs 模块
+import Mock from 'mockjs'
+// 把 json 数据格式引入进来
+// webpack 默认对外暴露的：图片、JSON数据格式
+import banner from './banner.json'
+import floor from './floor.json'
+
+// mock 数据：第一个参数（请求地址）  第二个参数（请求数据）
+Mock.mock('/mock/banner', {
+  code: 200,
+  data: banner
+}) // 模拟首页大的轮播图的数据
+
+Mock.mock('/mock/floor', {
+  code: 200,
+  data: floor
+}) // 模拟首页小的轮播图的数据
