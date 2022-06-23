18ddb6204788db6e4ed92c3720f828046f1693b8
liangchen926 <10736557+liangchen926@user.noreply.gitee.com>
2022-06-23 01:34:04 +0000

    Zoom

---
+++ src/pages/Detail/Zoom/Zoom.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="spec-preview">
+    <img :src="imgObj.imgUrl" />
+    <div class="event" @mousemove="handler"></div>
+    <div class="big">
+      <img :src="imgObj.imgUrl" ref="bigImg"/>
+    </div>
+    <!-- 遮罩层 -->
+    <div class="mask" ref="mask"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Zoom",
+  props: ["skuImageList"],
+  data() {
+    return {
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    // 先对 skuImageList[0] 进行计算，防止父组件返回的空数组导致报错
+    imgObj() {
+      return this.skuImageList[this.currentIndex] || {};
+    },
+  },
+  mounted() {
+    // 全局事件总线，获取兄弟组件传递过来的索引值
+    this.$bus.$on("getIndex", (index) => {
+      // 修改当前响应式数据
+      this.currentIndex = index;
+    });
+  },
+  methods: {
+    handler(event) {
+      let mask = this.$refs.mask;
+      let bigImg = this.$refs.bigImg;
+      let left = event.offsetX - mask.offsetWidth / 2;
+      let top = event.offsetY - mask.offsetHeight / 2;
+      // 约束范围
+      if(left <= 0) left = 0
+      else if(left >= mask.offsetWidth) left = mask.offsetWidth
+      if(top <= 0) top = 0
+      else if(top >= mask.offsetHeight) top = mask.offsetHeight
+      // 修改元素的left|top 属性值
+      mask.style.left = left + "px";
+      mask.style.top = top + "px";
+      bigImg.style.left = -left*2 + "px";
+      bigImg.style.top = -top*2 + "px";
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.spec-preview {
+  position: relative;
+  width: 400px;
+  height: 400px;
+  border: 1px solid #ccc;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  .event {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 998;
+  }
+
+  .mask {
+    width: 50%;
+    height: 50%;
+    background-color: rgba(188, 192, 188, 0.3);
+    position: absolute;
+    left: 0;
+    top: 0;
+    display: none;
+  }
+
+  .big {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: -1px;
+    left: 100%;
+    border: 1px solid #aaa;
+    overflow: hidden;
+    z-index: 998;
+    display: none;
+    background: white;
+
+    img {
+      width: 200%;
+      max-width: 200%;
+      height: 200%;
+      position: absolute;
+      left: 0;
+      top: 0;
+    }
+  }
+
+  .event:hover ~ .mask,
+  .event:hover ~ .big {
+    display: block;
+  }
+}
+</style>
